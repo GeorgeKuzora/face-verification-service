38f89ebc44e45d74bc271c83fc608d5603e89bcb
GeorgiyKuzora <rafale87@gmail.com>
2024-06-29 19:20:17 +0300

    Добавляет базовый функционал модуля main.py

--- src/app/main.py
+++ src/app/main.py
@@ -1,11 +1,19 @@
 import logging
 
+from app.service import FaceVerificationService
 
-def get_greetings() -> str:
-    """Возвращает строку приветствия."""
-    return 'Hello World!'
+
+def main() -> None:
+    """
+    Точка входа в программу.
+
+    Служит для задания точки входа в программу.
+    """
+    logging.basicConfig(level=logging.DEBUG)
+    fvs = FaceVerificationService()
+    img = 'src/tests/test_data/me.jpg'
+    logging.info(fvs.represent(img_path=img))
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
-    logging.info(get_greetings())
+    main()
