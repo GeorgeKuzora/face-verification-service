f196f520dbc5b7029de4ed46d7fa1283f2d46a27
GeorgiyKuzora <rafale87@gmail.com>
2024-07-06 19:10:10 +0300

    Добавляет фикстуру valid_tmp_file в модуль unit.test_service.py

--- src/tests/unit/test_service.py
+++ src/tests/unit/test_service.py
@@ -6,3 +6,15 @@ from pathlib import Path
 import pytest
 
 from app.service import FaceVerificationService, ModelName
+
+
+@pytest.fixture
+def valid_tmp_file(tmp_path):
+    """Фикстура для создания тестового файла."""
+    file_id = random.randint(1, 9)
+    file_name = f'temp{file_id}.txt'
+    with open(file_name, 'w'):
+        ...
+    file_path = tmp_path / file_name
+    yield file_path
+    os.remove(file_path)
