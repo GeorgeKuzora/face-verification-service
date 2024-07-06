a232c478b18e6bee3af6d6bf6eb0f881a2c88464
GeorgiyKuzora <rafale87@gmail.com>
2024-07-06 19:35:55 +0300

    Добавляет фикстуру tmp_file_valid в интеграционные тесты

--- src/tests/integration/test_service.py
+++ src/tests/integration/test_service.py
@@ -9,3 +9,23 @@ import pytest
 from app.service import FaceVerificationService, ModelName
 
 logger = logging.getLogger(__name__)
+
+
+@pytest.fixture
+def tmp_file_valid(tmp_path: Path):
+    """Фикстура для создания временного файла изображения."""
+    test_data_dir_path = 'src/tests/test_data/'
+    test_file_name = 'me.jpg'
+    test_file_path = Path(test_data_dir_path + test_file_name)
+
+    if not test_file_path.is_file():
+        logger.error(f'file {test_file_path} is not found')
+        raise OSError
+
+    tmp_file_path: Path = tmp_path / test_file_name
+    if tmp_file_path.is_file():
+        os.remove(tmp_file_path)
+
+    tmp_file = shutil.copy(test_file_path, tmp_file_path)
+    yield tmp_file
+    os.remove(tmp_file)
