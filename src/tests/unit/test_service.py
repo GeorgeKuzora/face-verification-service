7e34ecf1758fec90ac19a8b9ff2090794aaf8719
GeorgiyKuzora <rafale87@gmail.com>
2024-07-10 21:26:34 +0300

    Исправления согласно линтеру

--- src/tests/unit/test_service.py
+++ src/tests/unit/test_service.py
@@ -14,8 +14,8 @@ def valid_tmp_file(tmp_path):
     file_id = random.randint(1, 9)
     file_name = f'temp{file_id}.txt'
     file_path = tmp_path / file_name
-    with open(file_path, 'w') as file:
-        file.write("\n")
+    with open(file_path, 'w') as tmp_file:
+        tmp_file.write('\n')
     yield file_path
     os.remove(file_path)
 
