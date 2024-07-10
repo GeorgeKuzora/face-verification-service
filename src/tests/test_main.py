29abd4a04738f32ae1e07b96e1f1b5649b36d37e
GeorgiyKuzora <rafale87@gmail.com>
2024-07-10 21:06:06 +0300

    Revert "Удаляет тестовый модуль test_main.py"
    This reverts commit a55a0ec8f185a50ed1de872e0deb785410f55823.

---
+++ src/tests/test_main.py
@@ -0,0 +1,27 @@
+import os
+from pathlib import Path
+from typing import Any
+from app.service import FaceVerificationService
+import pytest
+
+
+@pytest.fixture
+def test_data_dir() -> str:
+    test_data_dir = 'src/tests/test_data/'
+    test_data_dir_path = Path(test_data_dir)
+    if not test_data_dir_path.is_dir():
+        os.mkdir(test_data_dir_path)
+    return test_data_dir
+
+
+@pytest.fixture
+def representation(test_data_dir: str) -> list[dict[str, Any]]:
+    img = 'me.jpg'
+    img_full_path = test_data_dir + img
+    service = FaceVerificationService()
+    return service.represent(img_full_path)
+
+
+def test_representation_is_not_empty(representation: list[dict[str, Any]]) -> None:
+    assert representation is not None
+    assert len(representation) > 0
