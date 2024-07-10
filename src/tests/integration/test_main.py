2f6ee3650a1373313994b47d82d2a48caaea8c19
GeorgiyKuzora <rafale87@gmail.com>
2024-07-10 21:26:58 +0300

    Удаляет лишние тесты из test_main

--- src/tests/integration/test_main.py
+++ src/tests/integration/test_main.py
@@ -1,32 +1,6 @@
-import os
-from pathlib import Path
-from typing import Any
-from app.service import FaceVerificationService
 from app.main import main
-import pytest
-
-
-@pytest.fixture
-def test_data_dir() -> str:
-    test_data_dir = 'src/tests/test_data/'
-    test_data_dir_path = Path(test_data_dir)
-    if not test_data_dir_path.is_dir():
-        os.mkdir(test_data_dir_path)
-    return test_data_dir
-
-
-@pytest.fixture
-def representation(test_data_dir: str) -> list[dict[str, Any]]:
-    img = 'me.jpg'
-    img_full_path = test_data_dir + img
-    service = FaceVerificationService()
-    return service.represent(img_full_path)
-
-
-def test_representation_is_not_empty(representation: list[dict[str, Any]]) -> None:
-    assert representation is not None
-    assert len(representation) > 0
 
 
 def test_main_without_error():
+    """Тестирует вызов main не вызывает ошибок."""
     main()
