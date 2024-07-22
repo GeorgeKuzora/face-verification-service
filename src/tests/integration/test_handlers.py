ca618dec66eec0c68f4b804ebd6081adf5a49447
GeorgiyKuzora <rafale87@gmail.com>
2024-07-22 13:11:28 +0000

    Добавляет интеграционные тесты

---
+++ src/tests/integration/test_handlers.py
@@ -0,0 +1,18 @@
+import pytest
+from fastapi import status
+from fastapi.testclient import TestClient
+
+from app.api.handlers import app
+
+
+@pytest.fixture
+def client():
+    """Тестовый клиент."""
+    return TestClient(app)
+
+
+def test_root(client):
+    """Тестирует корень."""
+    response = client.get('/')
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {'message': 'server is running'}
--- src/tests/integration/test_service.py
+++
@@ -1,6 +0,0 @@
-from app.service import main
-
-
-def test_main_without_error():
-    """Тестирует вызов main не вызывает ошибок."""
-    main()
