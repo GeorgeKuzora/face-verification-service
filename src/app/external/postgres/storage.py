90dd67b69e4e4ad13223e0f87bd87350480252a9
GeorgiyKuzora <rafale87@gmail.com>
2024-08-15 08:04:57 +0000

    Добавляет имплементацию подключения к postgres в классе DBStorage

---
+++ src/app/external/postgres/storage.py
@@ -0,0 +1,73 @@
+import logging
+from typing import Any
+
+from sqlalchemy import Engine, create_engine, select
+from sqlalchemy.orm import Session
+
+from app.core import models as srv
+from app.core.config import get_settings
+from app.external.postgres import models as db
+
+logger = logging.getLogger(__name__)
+
+
+def create_pool() -> Engine:
+    """Создет sqlalchemy engine с пулом соединений."""
+    settings = get_settings()
+    return create_engine(
+        str(settings.postgres.pg_dns),
+        pool_size=settings.postgres.pool_size,
+        max_overflow=settings.postgres.max_overflow,
+    )
+
+
+def create_all_tables() -> None:
+    """Создает таблицы в базе данных."""
+    pool = create_pool()
+    db.Base.metadata.create_all(pool)
+
+
+class DBStorage:
+    """База данных."""
+
+    def __init__(self) -> None:
+        """Метод инициализации."""
+        self.pool = create_pool()
+
+    def update_user(
+        self, vector: list[dict[str, Any]], username: str,
+    ) -> srv.User | None:
+        """
+        Метод обновления пользователя.
+
+        Устанавливает поле is_verified на true.
+
+        :param username: Имя пользователя
+        :type username: str
+        :param vector: Вектор лица пользователя
+        :type vector: list[dict[str, Any]]
+        :return: Обновленный пользователь.
+        :rtype: srv.User | None
+        """
+        with Session(self.pool) as session:
+            user = self._get_user(username, session)
+            if not user or user.is_deleted is True:
+                logger.error(f'{username} not found')
+                return None
+            user.is_verified = True
+            logger.info(f'{username}.is_verified set to True')
+            srv_user = self._get_srv_user(user)
+            session.commit()
+        return srv_user
+
+    def _get_user(self, username, session: Session) -> db.User | None:
+        return session.scalars(
+            select(db.User).where(db.User.username == username),
+        ).first()
+
+    def _get_srv_user(self, db_user: db.User) -> srv.User:
+        return srv.User(
+            username=db_user.username,
+            is_verified=db_user.is_verified,
+            user_id=db_user.id,
+        )
